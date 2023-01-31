84aa6143066c7d36ac01ba267cba735f14b63bdc
Maxwell Volz <maxwellvolz@gmail.com>
2023-01-31 15:08:29 -0800

    fixed background, video in 3d space

--- src/App.js
+++ src/App.js
@@ -9,36 +9,107 @@ import {
   Environment,
   OrbitControls,
   SpotLight,
-  useDepthBuffer
+  Lightformer,
+  useDepthBuffer,
+  MeshReflectorMaterial
 } from '@react-three/drei';
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper'
 
-import HutModel from './components/Hut';
+// import HutModel from './components/Hut';
+// import HutModel from './components/Hut3';
+// import HutModel from './components/Hut5';
+import HutModel from './components/Hut6';
 
 export default function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 10, 10], fov: 50 }}>
-      <group position={[0, -0.65, 0]}>
+    <Canvas shadows camera={{ position: [0, 20, 10], fov: 50 }}>
+
+      <color attach="background" args={['black']} />
+      <group position={[0, 0, 0]}>
         <Sphere />
         <Hut />
-        <AccumulativeShadows temporal frames={200} color="purple" colorBlend={0.5} opacity={1} scale={30} alphaTest={0.85}>
+        {/* <AccumulativeShadows temporal frames={200} color="purple" colorBlend={0.5} opacity={1} scale={30} alphaTest={0.85}>
           <RandomizedLight amount={8} radius={5} ambient={0.5} position={[5, 3, 2]} bias={0.001} />
-        </AccumulativeShadows>
+        </AccumulativeShadows> */}
       </group>
-      <fog attach="fog" args={['#202020', 5, 20]} />
-      <ambientLight intensity={0.015} />
-      <Env />
-      <OrbitControls autoRotate={false} autoRotateSpeed={1} enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.1} maxPolarAngle={Math.PI / 2.1} />
+      {/* <fog attach="fog" args={['#202020', 5, 20]} /> */}
 
-      <DopeLights />
+      <ambientLight intensity={0.025} />
 
-      {/* <mesh position={[0, -1.03, 0]} castShadow receiveShadow geometry={nodes.dragon.geometry} material={materials['Default OBJ.001']} dispose={null} /> */}
-      {/* <mesh receiveShadow position={[0, -1, 0]} rotation-x={-Math.PI / 2}>
+      {/* <Env /> */}
+
+      <OrbitControls autoRotate={false} autoRotateSpeed={1} enablePan={false} enableZoom={true} minPolarAngle={Math.PI / 4} maxPolarAngle={Math.PI / 2.1} />
+
+      {/* <DopeLights /> */}
+
+
+      {/* Lights */}
+      {/* <directionalLight position={[1, 5, 1]} color="red" />
+      <directionalLight position={[1, 10, 1]} color="blue" /> */}
+
+
+      <CrazyLight />
+      {/* <Lightformer form="circle" intensity={10} position={[1, 2.5, 1]} /> */}
+
+
+      {/* <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
         <planeGeometry args={[50, 50]} />
         <meshPhongMaterial />
       </mesh> */}
 
+      <ReflectiveGround />
+
     </Canvas>
+  )
+}
+
+function CrazyLight() {
+  const light = useRef()
+  const light_target = useRef()
+  let light_intensity = 0
+
+  useFrame((state, delta) => {
+    const t = (1 + Math.sin(state.clock.elapsedTime * 2)) / 2
+    // stripe.current.color.setRGB(1 + t * 10, 2, 20 + t * 50)
+    // easing.dampE(head.current.rotation, [0, state.pointer.x * (state.camera.position.z > 1 ? 1 : -1), 0], 0.4, delta)
+    light.current.intensity = 1 + t * 6
+    light.current.distance = 5 + t * 1
+    // light_intensity = 1 + t * 2
+    // console.log(light.current.intensity)
+  })
+
+  return (
+    <group>
+      <mesh ref={light_target} position={[3.83, 1.7, -3.27]} />
+
+      <SpotLight ref={light} penumbra={1} distance={5} angle={0.7} anglePower={4} intensity={1} position={[4.6, 20, -2.01]}>
+
+      </SpotLight>
+
+    </group>
+
+
+  )
+}
+function ReflectiveGround() {
+  const { roughness } = useControls({ roughness: { value: 1, min: 0, max: 1 } })
+  return (
+    <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry args={[50, 50]} />
+      <MeshReflectorMaterial
+        blur={[300, 30]}
+        resolution={2048}
+        mixBlur={1}
+        mixStrength={80}
+        roughness={1}
+        depthScale={1.2}
+        minDepthThreshold={0.4}
+        maxDepthThreshold={1.4}
+        color="#202020"
+        metalness={0.8}
+      />
+    </mesh>
   )
 }
 
@@ -56,10 +127,17 @@ function Sphere() {
 
 function DopeLights() {
   const depthBuffer = useDepthBuffer({ frames: 1 })
+
+  const pos = useControls({
+    x: { value: -1.6, min: -10, max: 10 },
+    y: { value: 5.7, min: -10, max: 10 },
+    z: { value: -1.3, min: -10, max: 10 }
+  })
+
   return (
     <mesh>
-      <MovingSpot depthBuffer={depthBuffer} color="#0c8cbf" position={[1.8, 1.8, -1]} />
-      <MovingSpot depthBuffer={depthBuffer} color="#b00c3f" position={[1.7, 1.8, -1.1]} />
+      <MovingSpot depthBuffer={depthBuffer} color="#0c8cbf" position={[pos.x, pos.y, pos.z]} />
+      <MovingSpot depthBuffer={depthBuffer} color="#b00c3f" position={[pos.x - .05, pos.y, pos.z - .05]} />
     </mesh>
   )
 }
@@ -69,7 +147,7 @@ function Hut() {
 }
 
 function Env() {
-  const [preset, setPreset] = useState('sunset')
+  const [preset, setPreset] = useState('park')
   // You can use the "inTransition" boolean to react to the loading in-between state,
   // For instance by showing a message
   const [inTransition, startTransition] = useTransition()
@@ -94,5 +172,5 @@ function MovingSpot({ vec = new Vector3(), ...props }) {
     light.current.target.position.lerp(vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0), 0.1)
     light.current.target.updateMatrixWorld()
   })
-  return <SpotLight castShadow ref={light} penumbra={1} distance={6} angle={0.35} attenuation={5} anglePower={4} intensity={2} {...props} />
+  return <SpotLight castShadow ref={light} penumbra={1} distance={6} angle={0.35} attenuation={5} anglePower={4} intensity={3} {...props} />
 }
